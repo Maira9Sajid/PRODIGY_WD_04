0ffb88379fe6c03c04f7785226516b4b7b81c610
Maira Sajid <storemagnesium@gmail.com>
2024-07-27 17:08:06 +0500

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,9 @@
+var typed = new Typed('.text', {
+    strings: ["Web Developer", "SEO Expert", "Graphic Designer"],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true
+});
+
+
